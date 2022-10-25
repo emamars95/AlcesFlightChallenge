12d124f3b721d00036ad61a85664bcc22a84efa1
emamars95 <115721827+emamars95@users.noreply.github.com>
2022-10-25 16:33:18 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -11,3 +11,18 @@ conda install -c conda-forge gcc
 export OMP_NUM_THREADS=2 
 gcc -fopenmp -D_OPENMP -o3 -Ofast -mtune=native -DSTREAM_ARRAY_SIZE=60000000 stream.c -o stream
 
+conda install -c "conda-forge/label/openmpi_rc" openmpi-mpicc << EOF
+y
+EOF 
+conda install -c conda-forge libcblas << EOF
+y
+EOF
+wget https://netlib.org/benchmark/hpl/hpl-2.3.tar.gz 
+tar -xf hpl-2.3.tar.gz 
+# ---------------------
+# cd hpl-2.3 
+# cp setup/Make.Linux_PII_CBLAS .
+# Edit the file Make.Linux_PII_CBLAS with the main root for openmpi
+# ./configure
+# make arch=Linux_PII_CBLAS
+# ---------------------- 
