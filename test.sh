8de48b30511ab9981184cfde4b05934888307d69
emamars95 <115721827+emamars95@users.noreply.github.com>
2022-10-26 18:46:13 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum group install "Development Tools" << EOF
+y
+EOF
+sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget https://www.cs.virginia.edu/stream/FTP/Code/stream.c
