6b0748921edd966e29b9af2a6671de2952e41a73
emamars95 <115721827+emamars95@users.noreply.github.com>
2022-10-27 11:58:17 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,10 @@
 sudo yum group install "Development Tools" << EOF
 y
 EOF
+sudo yum install wget << EOF
+y
+EOF
 sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ~/miniconda.sh -p $HOME/miniconda
+
 wget https://www.cs.virginia.edu/stream/FTP/Code/stream.c
