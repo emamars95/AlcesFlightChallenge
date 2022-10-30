e0d442ef5380cfcdd878adc85742170be89e31b4
emamars95 <115721827+emamars95@users.noreply.github.com>
2022-10-30 15:03:39 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ rm -rf Miniconda3-latest-Linux-x86_64.sh << EOF
 y
 EOF
 sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
-bash ~/Miniconda3-latest-Linux-x86_64.sh -p $HOME/miniconda
+bash $HOME/Miniconda3-latest-Linux-x86_64.sh -p $HOME/miniconda
 
 rm -f conda.txt
 wget https://raw.githubusercontent.com/emamars95/CIUK/main/conda.txt >> config.log
