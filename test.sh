f663f21e8a7eae819e5cc6b8cc5f9aeec494b774
emamars95 <115721827+emamars95@users.noreply.github.com>
2022-10-30 14:51:13 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ~/miniconda.sh -p $HOME/miniconda
 
 wget https://raw.githubusercontent.com/emamars95/CIUK/main/conda.txt
-conda create -n ENVNAME --file ENV.txt
+conda create -n ENVNAME --file conda.txt
 
 eval "$(conda shell.bash hook)"
 conda activate <env-name>
