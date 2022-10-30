ece45486a00cfeb6f5e23e69ac1c52e01f36b39e
emamars95 <115721827+emamars95@users.noreply.github.com>
2022-10-30 15:36:26 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,10 +7,12 @@ rm -rf Miniconda3-latest-Linux-x86_64.sh << EOF
 y
 EOF
 sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
-bash $HOME/Miniconda3-latest-Linux-x86_64.sh -p $HOME/miniconda
+bash $HOME/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+./$HOME/miniconda/condabin/conda init
+source ~/.bashrc
 
 rm -f conda.txt
-wget https://raw.githubusercontent.com/emamars95/CIUK/main/conda.txt >> config.log
+wget https://raw.githubusercontent.com/emamars95/CIUK/main/conda.txt 
 conda create -n challenge --file conda.txt 
 
 eval "$(conda shell.bash hook)"
