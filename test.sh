589674fd05d927cf932aff468825414bf9030ab4
emamars95 <115721827+emamars95@users.noreply.github.com>
2022-10-30 14:51:38 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ~/miniconda.sh -p $HOME/miniconda
 
 wget https://raw.githubusercontent.com/emamars95/CIUK/main/conda.txt
-conda create -n ENVNAME --file conda.txt
+conda create -n challenge --file conda.txt
 
 eval "$(conda shell.bash hook)"
-conda activate <env-name>
+conda activate challenge
 
 wget https://www.cs.virginia.edu/stream/FTP/Code/stream.c
