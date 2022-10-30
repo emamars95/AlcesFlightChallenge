3f79333ba1aa1b4e5a7e96aca0823ea5950cd92a
emamars95 <115721827+emamars95@users.noreply.github.com>
2022-10-30 14:43:11 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,4 +6,9 @@ EOF
 sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ~/miniconda.sh -p $HOME/miniconda
 
+
+
+eval "$(conda shell.bash hook)"
+conda activate <env-name>
+
 wget https://www.cs.virginia.edu/stream/FTP/Code/stream.c
